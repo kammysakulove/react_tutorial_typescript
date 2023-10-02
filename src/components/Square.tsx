a9c3bfeca824de65c21aadd32d14bff9b07c8dda
kammysakulove <sugimoto@anchorworks.co.jp>
2023-10-02 15:51:01 +0900

    ファイルの分割

---
+++ src/components/Square.tsx
@@ -0,0 +1,14 @@
+type SquareProps = {
+  value: string;
+  onSquareClick: () => void;
+};
+
+const Square: React.FC<SquareProps> = ({ value, onSquareClick }) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
